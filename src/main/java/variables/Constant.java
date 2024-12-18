97f3d6f6c36c959984477b0a7e799a655b8769e7
HariusNguyen <insuffiecentminh04@gmail.com>
2024-12-18 23:21:14 +0700

    update more variables

--- src/main/java/variables/Constant.java
+++ src/main/java/variables/Constant.java
@@ -5,5 +5,11 @@ public class Constant {
     public static final int MAX_SCREEN_ROW = 15;
     public static final int SCALE = 3;
     public static final int ORIGINAL_TILE_SIZE = 16;
-    public static final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
+    public static final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE; // 48
+    public static final int WIDTH = TILE_SIZE * MAX_SCREEN_COL; // 960
+    //Player position in main screen
+    public static final int HEIGHT = TILE_SIZE * MAX_SCREEN_ROW; // 720
+    public static final String TITLE = "Catch Me If You Can";
+    public static final int FPS = 60;
+    //public static final int Tera= 1_000_000_000;
 }
