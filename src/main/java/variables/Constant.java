4684914533413a9e0ac16f6299077b0e26d51eab
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-12-18 14:55:18 +0700

    add tile size

--- src/main/java/variables/Constant.java
+++ src/main/java/variables/Constant.java
@@ -1,8 +1,9 @@
-package variable;
+package variables;
 
 public class Constant {
     public static final int MAX_SCREEN_COL = 20;
     public static final int MAX_SCREEN_ROW = 15;
     public static final int SCALE = 3;
     public static final int ORIGINAL_TILE_SIZE = 16;
+    public static final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
 }
