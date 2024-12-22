5eabaf6aa80048e75b1287a9fddff276da20793d
Khang Huy <khatihuy125@gmail.com>
2024-12-23 00:58:58 +0700

    add check collision and  search path function for boss

--- src/main/java/entities/Boss.java
+++ src/main/java/entities/Boss.java
@@ -1,5 +1,8 @@
 package entities;
 
+import config.Game;
+import core.Collision;
+import features.PathFinding;
 import variables.Constant;
 
 import java.awt.*;
@@ -8,6 +11,8 @@ import java.io.IOException;
 
 public class Boss extends Entity {
     public boolean collision;
+    public Collision collisionCheck = new Collision();
+    public PathFinding pFind = new PathFinding();
 
     public Boss() {
         this.name = "boss";
@@ -39,12 +44,103 @@ public class Boss extends Entity {
         }
     }
 
+    public void checkCollision() {
+        collisionOn = false;
+        collisionCheck.checkTile(this);
+        collisionCheck.checkMob(Game.getPlayer(), this);
+    }
 
     @Override
     public void update() {
+        int startCol = (Game.getPlayer().x + Game.getPlayer().solidArea.x) / Constant.TILE_SIZE;
+        int startRow = (Game.getPlayer().y + Game.getPlayer().solidArea.y) / Constant.TILE_SIZE;
+        checkCollision();
+        searchPath(startCol, startRow);
+
+        spriteCounter++;
+        if (spriteCounter > 8) {
+            if (spriteNum != 4) {
+                spriteNum++;
+            } else
+                spriteNum = 1;
+            spriteCounter = 0;
+        }
 
     }
 
+    public void searchPath(int goalCol, int goalRow) {
+        int startCol = (this.x + solidArea.x) / Constant.TILE_SIZE;
+        int startRow = (this.y + solidArea.y) / Constant.TILE_SIZE;
+        pFind.setNodes(startCol, startRow, goalCol, goalRow);
+
+        if (pFind.search()) {
+            int nextX = pFind.pathList.get(0).col * Constant.TILE_SIZE;
+            int nextY = pFind.pathList.get(0).row * Constant.TILE_SIZE;
+
+            setEntityInteractionBox(this);
+
+            if (this.InteractionBox.get(0) > nextY &&
+                    this.InteractionBox.get(3) >= nextX &&
+                    this.InteractionBox.get(1) < nextX + Constant.TILE_SIZE) {
+                direction = 0;
+                y -= speed;
+            } else if (this.InteractionBox.get(0) < nextY &&
+                    this.InteractionBox.get(3) >= nextX &&
+                    this.InteractionBox.get(1) < nextX + Constant.TILE_SIZE) {
+                direction = 2;
+                y += speed;
+            } else if (this.InteractionBox.get(0) >= nextY &&
+                    this.InteractionBox.get(2) <= nextY + Constant.TILE_SIZE) {
+                //Left or Right
+                if (this.InteractionBox.get(3) > nextX) {
+                    direction = 3;
+                    x -= speed;
+                }
+                if (this.InteractionBox.get(3) < nextX) {
+                    direction = 1;
+                    x += speed;
+                }
+            } else if (this.InteractionBox.get(0) > nextY && this.InteractionBox.get(3) > nextX) {
+                //Up or Left
+                direction = 0;
+                y -= speed;
+                checkCollision();
+                if (collisionOn) {
+                    direction = 3;
+                    x -= speed;
+                }
+
+            } else if (this.InteractionBox.get(0) > nextY && this.InteractionBox.get(3) < nextX) {
+                //Up or Right
+                direction = 0;
+                y -= speed;
+                checkCollision();
+                if (collisionOn) {
+                    direction = 1;
+                    x += speed;
+                }
+            } else if (this.InteractionBox.get(0) < nextY && this.InteractionBox.get(3) > nextX) {
+                //Down or Left
+                direction = 2;
+                y += speed;
+                checkCollision();
+                if (collisionOn) {
+                    direction = 3;
+                    x -= speed;
+                }
+
+            } else if (this.InteractionBox.get(0) < nextY && this.InteractionBox.get(3) < nextX) {
+                //Down or Right
+                direction = 2;
+                y += speed;
+                checkCollision();
+                if (collisionOn) {
+                    direction = 1;
+                    x += speed;
+                }
+            }
+        }
+    }
 
     @Override
     public void draw(Graphics2D g2) {
